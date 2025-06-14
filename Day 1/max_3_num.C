ec710891bb70258b5d9528d98410ecd6d7c8e26d
sivadharanisubburaman <163972331+sivadharanisubburaman@users.noreply.github.com>
2025-06-14 10:51:15 +0530

    Create max_3_num.C

---
+++ Day 1/max_3_num.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+    int a=5,b=7,c=4;
+    if(a>b &&a>c)
+    {
+        printf("A is greater");
+    }
+    else if (b>a &&b>c)
+    {
+        printf("B is greater");
+    }
+    else
+    {
+    printf("C is greater");
+    }
+    
+    
+    return 0;
+}
