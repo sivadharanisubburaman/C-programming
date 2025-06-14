b393866dc7e837dfa994239d3f87baf89e1c5f7a
sivadharanisubburaman <163972331+sivadharanisubburaman@users.noreply.github.com>
2025-06-14 11:11:12 +0530

    Create Hour_Minutes.c

---
+++ Day3/Hour_Minutes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int isValidTime(int hour, int minute) {
+    if (hour >= 0 && hour <= 23 && minute >= 0 && minute <= 59)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    int hour, minute;
+
+    printf("Enter time (HH:MM): ");
+    scanf("%d:%d", &hour, &minute);
+
+    if (isValidTime(hour, minute))
+        printf("Valid Time\n");
+    else
+        printf("Invalid Time\n");
+
+    return 0;
+}
